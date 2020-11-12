f6a2f9a429f5b6e8b412323cbbbcd747580d636a
CyrilleKolle <ngidekollecyrille@gmail.com>
2020-11-12 10:24:21 +0100

    added platform specific text

--- TodoList/TodoList/Views/TodoListView.xaml.cs
+++ TodoList/TodoList/Views/TodoListView.xaml.cs
@@ -14,6 +14,28 @@ namespace TodoList.Views
             BindingContext = new TodoListViewModel(Navigation);
             ThemeInfo.Text = $"The current app theme is {AppInfo.RequestedTheme}";
             Vibration.Vibrate();
+           
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                PlatformX.Text = "You are using an android mobile";
+                var image = new Image { Source = "@Images/tododroid.png" };
+                //image.Source = Device.RuntimePlatform;
+                // ImageSource.FromFile("@Images/tododroid");
+                
+                 
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                PlatformX.Text = "You are using an ios mobile";
+                var image = new Image { Source = "@Images/todoios.png" };
+                //image.Source = Device.RuntimePlatform;
+                // ImageSource.FromFile("@Images/todoios.png");
+
+            }
+        }
+        public string SomeImage
+        {
+            get { return string.Format("@Images/tododroid.png"); }
         }
 
         protected override async void OnAppearing()
